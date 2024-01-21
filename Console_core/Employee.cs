1eae21894815813c6d473d01229f25e0d831b61d
skcore <skcore1013@gmail.com>
2024-01-21 18:02:31 +0530

    serialization - Json Ignore

---
+++ Console_core/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Json_ignore
+{
+    internal class Employee
+    {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public int ID { get; set; }
+        public string? Name { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string? City { get; set; }
+        public int RefIdReadOnly { get; private set; } = 1013;
+    }
+}
---
+++ Console_core/Program.cs
@@ -0,0 +1,97 @@
+﻿using Json_ignore;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+Employee employee = new()
+{
+    ID = 1,
+    Name = "Jhon",
+    City = "G-City"
+
+};
+
+/*
+ * Json Serialization without options
+ */
+
+string empJson = JsonSerializer.Serialize(employee);
+
+Console.WriteLine(empJson);
+
+/*Output
+ * {"ID":1,"Name":"Jhon","City":"G-Capital","RefIdReadOnly":1013}
+ */
+
+
+
+/*
+ * Json Serialization - To ignore individual properties 
+ * City - JsonIgnore
+ */
+
+empJson = JsonSerializer.Serialize(employee);
+
+Console.WriteLine(empJson);
+
+/*Output
+ * {"ID":1,"Name":"Jhon","RefIdReadOnly":1013}
+ */
+
+
+/*
+ * We can specify conditional exclusion by setting the [JsonIgnore] attribute's Condition property
+ * City - null
+ * JsonIgnoreCondition.WhenWritingDefault, 
+ *  JsonIgnoreCondition.WhenWritingNull
+ */
+
+empJson = JsonSerializer.Serialize(employee);
+
+Console.WriteLine(empJson);
+
+/* Output 
+ * {"Name":"Jhon","RefIdReadOnly":1013}
+ */
+
+
+/*
+ * Ignore all read-only properties
+ * 
+ */
+
+var options = new JsonSerializerOptions
+{
+
+    IgnoreReadOnlyProperties = true
+
+};
+empJson = JsonSerializer.Serialize(employee, options);
+
+Console.WriteLine(empJson);
+
+/* Output 
+ * {"ID":0,"Name":"Jhon","City":"G-Capital"}
+ */
+
+
+
+/*
+ * Ignore all null-value properties
+ * Pass city = null
+ */
+
+JsonSerializerOptions jsonSerializerOptions = new()
+{
+    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+};
+
+empJson = JsonSerializer.Serialize(employee, jsonSerializerOptions);
+
+Console.WriteLine(empJson);
+
+/* Output 
+ * {"ID":0,"Name":"Jhon","RefIdReadOnly":1013}
+ */
+
+
+
